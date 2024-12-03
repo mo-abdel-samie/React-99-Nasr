acc2528fa3743e5aab5831cecffa55e9a23a0a56
mo-abdel-samie <mohamed.abdelsamie3009@gmail.com>
2024-12-03 21:47:13 +0200

    Update brand name

--- src/components/MainNavbar.jsx
+++ src/components/MainNavbar.jsx
@@ -9,7 +9,7 @@ export default function MainNavbar() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">React Store</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto align-self-center">
